ceb190ef1efa124b296e02aaa30eb98060d522c7
kokdot <kok.74@mail.ru>
2025-06-02 20:47:38 +0500

    add mysql

--- mysql_source.sh
+++ mysql_source.sh
@@ -1,2 +1,2 @@
-cp mysqld.cnf /etc/mysql/mysql.conf.d
+cp mysql/mysqld.cnf /etc/mysql/mysql.conf.d
 mysql -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020'; GRANT REPLICATION SLAVE ON *.* TO repl@'%'; FLUSH PRIVILEGES;SELECT User, Host FROM mysql.user;FLUSH TABLES WITH READ LOCK;SHOW MASTER STATUS;"
