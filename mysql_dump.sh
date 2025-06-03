8bef7a27596eb3184010b53794307caa56965996
kokdot <kok.74@mail.ru>
2025-06-03 23:40:54 +0500

    mysql_dump.sh

---
+++ mysql_dump.sh
@@ -0,0 +1,13 @@
+!#/etc/bash
+
+for s in `mysql -e "SHOW DATABASES"`;
+do
+#mkdir $s;
+for t in `mysql -e "use $s; show tables;"`;
+do
+/usr/bin/mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $s $t > $s/$t;
+
+#echo $t;
+done
+done
+rm -rf Database
