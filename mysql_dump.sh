04f629d2f9b04b79c56c2f32450d48797a7113d0
kokdot <kok.74@mail.ru>
2025-06-03 23:54:32 +0500

    fix dump.sh

--- mysql_dump.sh
+++ mysql_dump.sh
@@ -1,8 +1,8 @@
-!#/etc/bash
+#!/etc/bash
 
 for s in `mysql -e "SHOW DATABASES"`;
 do
-#mkdir $s;
+mkdir $s;
 for t in `mysql -e "use $s; show tables;"`;
 do
 /usr/bin/mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $s $t > $s/$t;
