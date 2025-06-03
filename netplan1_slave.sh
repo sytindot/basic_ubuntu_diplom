0dc56caef34876d9f9ef28f47e57026a0a9ef5d7
kokdot <kok.74@mail.ru>
2025-06-03 17:43:34 +0500

    fix netplan

--- netplan1_slave.sh
+++ netplan1_slave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash +x
 
 netplan_file='netplan/9999-network3.yaml'
-cd ~/basic_ubuntu_diplom
+cd /home/user/basic_ubuntu_diplom
 if (( "$?" != 0 )); then
 	echo "Ошибка при попытке перехода в каталог basic_ubuntu_diplom"
 	exit 1
