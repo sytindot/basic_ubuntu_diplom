a2019e99f6c11881a5744167fe160d00eb2fe8f3
kokdot <kok.74@mail.ru>
2025-06-05 13:19:46 +0500

    elastic

--- install_elastic.sh
+++ install_elastic.sh
@@ -5,4 +5,10 @@ cd /home/user/elk-8.9-deb/
 dpkg -i elasticsearch-8.9.1-amd64.deb
 # sudo -i
 echo -e "-Xms1g\n-Xmx1g" >   /etc/elasticsearch/jvm.options.d/jvm.options
+cd /home/user/basic_ubuntu-diplom
+cp elasticsearch.yml /etc/elasticsearch/
+
+systemctl daemon-reload
+systemctl enable --now elasticsearch.service
+systemctl start elasticsearch.service
 
