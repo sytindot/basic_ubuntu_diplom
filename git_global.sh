c1884c51eb95676451a5ee2bac241e10ae375b5d
kokdot <kok.74@mail.ru>
2025-06-05 11:21:06 +0500

    git.global

---
+++ git_global.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config --global user.name "sytindot"
+git config --global user.email "nilolaysytin@yandex.ru"
---
+++ netplan_elk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash +x
+
+netplan_file='netplan/9999-network4.yaml'
+cd /home/user/basic_ubuntu_diplom
+if (( "$?" != 0 )); then
+	echo "Ошибка при попытке перехода в каталог basic_ubuntu_diplom"
+	exit 1
+fi
+# echo "check file netplan_file: " $netplan_file
+# echo "$(ls $netplan_file)"
+if [ -f "$netplan_file" ]; then
+	cp $netplan_file /etc/netplan
+	if (( "$?" == 0 )); then
+		netplan apply
+		sleep 5
+		netplan generate
+	fi
+
+#	echo $netplan_file "успешно скопирован"
+else 
+	echo "Обибка при копировании файла $netplan_file" 
+fi
+
