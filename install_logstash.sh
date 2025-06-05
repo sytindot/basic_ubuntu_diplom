2e9e0db7a688edeb935ef5b6c05fd06bb8dc6330
kokdot <kok.74@mail.ru>
2025-06-05 15:10:04 +0500

    logstash2

--- install_logstash.sh
+++ install_logstash.sh
@@ -5,8 +5,8 @@ cd /home/user/elk-8.9-deb/
 dpkg -i logstash-8.9.1-amd64.deb
 systemctl enable --now logstash.service
 cd /home/user
-cp logstash.yml etc/logstash
+cp logstash.yml /etc/logstash
 cp logstash-nginx-es.conf /etc/logstash/conf.d
 systemctl daemon-reload
-systemctl start logstesh.service
+systemctl start logstash.service
 
