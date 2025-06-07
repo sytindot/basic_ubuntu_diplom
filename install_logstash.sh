781d6085b7fc0454f4dcc6d50158656e18c549d0
kokdot <kok.74@mail.ru>
2025-06-07 08:19:00 +0500

    logstash

--- install_logstash.sh
+++ install_logstash.sh
@@ -4,7 +4,7 @@ cd /home/user/elk-8.9-deb/
 
 dpkg -i logstash-8.9.1-amd64.deb
 systemctl enable --now logstash.service
-cd /home/user
+cd /home/user/basic_ubuntu_diplom
 cp logstash.yml /etc/logstash
 cp logstash-nginx-es.conf /etc/logstash/conf.d
 systemctl daemon-reload
