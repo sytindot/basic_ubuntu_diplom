f838d0e57313f97ea40c900b8f4385e65a252c2b
kokdot <kok.74@mail.ru>
2025-06-07 08:08:23 +0500

    kibana

--- install_kibana.sh
+++ install_kibana.sh
@@ -3,9 +3,7 @@
 cd /home/user/elk-8.9-deb/
 
 dpkg -i kibana-8.9.1-amd64.deb
-#vim /etc/kibana/kibana.yml
-#server.host: "0.0.0.0"
-#:wq
+cp kibana.yml /etc/kibana
 systemctl daemon-reload
 systemctl enable --now kibana.service
 service kibana restart
