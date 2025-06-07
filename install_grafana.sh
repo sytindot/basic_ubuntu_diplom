2a90a86535acf71ca28188426d5b8e703d057b3e
kokdot <kok.74@mail.ru>
2025-06-07 06:21:06 +0500

    grafana1

--- install_grafana.sh
+++ install_grafana.sh
@@ -2,6 +2,7 @@ sudo apt-get install -y adduser libfontconfig1 musl
 # sudo apt --fix-broken install
 cd /home/user
 sudo dpkg -i grafana_11.6.1_amd64-224190-5beb3f.deb 
+systemctl enable --now grafana-server.service
 systemctl daemon-reload
-systemctl start grafana-server
+systemctl restart grafana-server.service
 
