7a09678460b6e7a710414e695679c7affe0c82fc
kokdot <kok.74@mail.ru>
2025-06-09 11:46:16 +0500

    fix mysql

--- mysql_create_full_dump.sh
+++ mysql_create_full_dump.sh
@@ -2,4 +2,4 @@
 
 mkdir -p  /home/user/database/dump
 prefix=$(date --rfc-3339=date)
-mysqldump --master-data employees > /home/user/database/dump/employees_dump${prefix}.sql
+mysqldump --master-data  --all-databases --triggers --routines --events employees > /home/user/database/dump/employees_dump${prefix}.sql
