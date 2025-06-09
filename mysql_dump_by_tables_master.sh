6c1898b0cd6655d595da682ac18c540810a7889e
kokdot <kok.74@mail.ru>
2025-06-09 12:22:53 +0500

    fix mysql2

---
+++ mysql_dump_by_tables_master.sh
@@ -0,0 +1,22 @@
+#!/etc/bash
+
+
+mkdir -p  /home/user/database/dump
+
+cd /home/user/database/dump
+
+
+for s in `mysql -e "SHOW DATABASES"`;
+do
+if [[ $s -eq 'Database' ]]; then
+	continue
+fi
+mkdir $s;
+for t in `mysql -e "use $s; show tables;"`;
+do
+/usr/bin/mysqldump --all-databases --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $s $t > $s/$t;
+
+#echo $t;
+done
+done
+#rm -rf Database
