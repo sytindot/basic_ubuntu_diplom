6b910af174d67c1623442abe908c0fc0c3b309a7
kokdot <kok.74@mail.ru>
2025-06-09 14:42:54 +0500

    fix mysql2

--- mysql_create_full_dump.sh
+++ mysql_create_full_dump.sh
@@ -2,5 +2,4 @@
 
 mkdir -p  /home/user/database/dump
 prefix=$(date --rfc-3339=date)
-mysqldump employees > /home/user/database/dump/employees_dump${prefix}.sql
-#mysqldump --master-data  --all-databases --triggers --routines --events employees > /home/user/database/dump/employees_dump${prefix}.sql
+mysqldump  --set-gtid-purged=OFF --all-databases --triggers --routines --events employees > /home/user/database/dump/employees_dump${prefix}.sql
