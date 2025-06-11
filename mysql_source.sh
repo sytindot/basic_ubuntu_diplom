b41cb02f7a47056bac69e0170215e4f6bffb2821
kokdot <kok.74@mail.ru>
2025-06-11 12:47:51 +0500

    sourse

--- mysql_source.sh
+++ mysql_source.sh
@@ -1,2 +1,4 @@
-cp mysql/mysqld.cnf /etc/mysql/mysql.conf.d
+#!/bin/bash
+
+
 mysql -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020'; GRANT REPLICATION SLAVE ON *.* TO repl@'%'; FLUSH PRIVILEGES;SELECT User, Host FROM mysql.user;FLUSH TABLES WITH READ LOCK;SHOW MASTER STATUS;"
