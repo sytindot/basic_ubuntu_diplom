e30984bf4bbb2dcfb9b5468521651bf78a795923
kokdot <kok.74@mail.ru>
2025-06-12 20:04:52 +0500

    remome

--- install_elastik.sh
+++ install_elastik.sh
@@ -1,15 +1,17 @@
-dpkg -i elasticsearch-8.9.1-amd64.deb
-sudo -i
-echo -e "-Xms1g\n-Xmx1g" >   /etc/elasticsearch/jvm.options.d/jvm.options
-vim jvm.options
--Xms1g
--Xmx1g
+
 cd /home/user/elk-8.9-deb/
-vim /etc/elasticsearch/elasticsearch.yml
-/xpack.se
-4 false
-http.host: 0.0.0.0
-:wq
+dpkg -i elasticsearch-8.9.1-amd64.deb
+#sudo -i
+echo -e "-Xms1g\n-Xmx1g" >   /etc/elasticsearch/jvm.options.d/jvm.options
+#vim jvm.options
+#-Xms1g
+#-Xmx1g
+#cd /home/user/elk-8.9-deb/
+#vim /etc/elasticsearch/elasticsearch.yml
+#/xpack.se
+#4 false
+#http.host: 0.0.0.0
+#:wq
 systemctl daemon-reload
 systemctl enable --now elasticsearch.service
 curl http://localhost:9200
