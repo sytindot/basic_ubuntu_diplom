2aef16dcc1457a781c542475185f204bfe0d2e33
kokdot <kok.74@mail.ru>
2025-06-12 18:14:36 +0500

    elk

--- download_elk.sh
+++ download_elk.sh
@@ -3,3 +3,4 @@
 
 cd /home/user/Downloads
 scp  elk_8.9_deb-224190-ae521e.zip user@192.168.0.223:/home/user
+scp grafana_11.6.1_amd64-224190-5beb3f.deb user@192.168.0.223:/home/user
--- install_grafana.sh
+++ install_grafana.sh
@@ -1,4 +1,7 @@
-sudo apt-get install -y adduser libfontconfig1 musl
+#!/bin/bash
+
+
+#sudo apt-get install -y adduser libfontconfig1 musl
 # sudo apt --fix-broken install
 cd /home/user
 sudo dpkg -i grafana_11.6.1_amd64-224190-5beb3f.deb 
