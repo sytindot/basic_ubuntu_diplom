36b1352cb6ca5292384446a1c236699095afc375
kokdot <kok.74@mail.ru>
2025-06-12 19:15:57 +0500

    remome

--- install_kibana.sh
+++ install_kibana.sh
@@ -3,6 +3,7 @@
 cd /home/user/elk-8.9-deb/
 
 dpkg -i kibana-8.9.1-amd64.deb
+cd /home/user/basic_ubuntu_diplom
 cp kibana.yml /etc/kibana
 systemctl daemon-reload
 systemctl enable --now kibana.service
