106086eea4efd6f4b3196420a0078f577288c65c
kokdot <kok.74@mail.ru>
2025-06-13 00:45:50 +0500

    finish

--- elk_finish.sh
+++ elk_finish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ######################## elasticsearch
+echo "configuration elasticsearch"
 echo -e "-Xms1g\n-Xmx1g" >   /etc/elasticsearch/jvm.options.d/jvm.options
 cd /home/user/basic_ubuntu_diplom
 cp elasticsearch.yml /etc/elasticsearch
@@ -8,16 +9,19 @@ systemctl enable --now elasticsearch.service
 systemctl restart elasticsearch.service
 curl http://localhost:9200
 ######################## grafana
+echo "configuration grafana"
 systemctl enable --now grafana-server.service
 systemctl daemon-reload
 systemctl restart grafana-server.service
 ########################## kibana
+echo "configuration kibana"
 cd /home/user/basic_ubuntu_diplom
 cp kibana.yml /etc/kibana
-systemctl daemon-reload
 systemctl enable --now kibana.service
-service kibana restart
+systemctl daemon-reload
+systemctl restart kibana.service
 ########################### logshtash
+echo "configuration logshtash"
 cd /home/user/basic_ubuntu_diplom
 cp logstash.yml /etc/logstash
 cp logstash-nginx-es.conf /etc/logstash/conf.d
