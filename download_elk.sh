c4d8ff39045f35cda38351e0a318739f4799b00b
kokdot <kok.74@mail.ru>
2025-06-12 18:01:37 +0500

    elk

--- download_elk.sh
+++ download_elk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-cd /home/user/Downloads/elk-8.9-deb
-scp ./* user@192.168.0.220:/home/user/elk-8.9-deb/
+cd /home/user/Downloads
+scp  elk_8.9_deb-224190-ae521e.zip user@192.168.0.223:/home/user
--- elk1.sh
+++ elk1.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-apt install default-jdk -y
+#apt install default-jdk -y
 #apt install nginx -y
-apt install unzip -y
-apt install prometheus prometheus-node-exporter -y
-apt-get install -y adduser libfontconfig1 musl
+#apt install unzip -y
+#apt install prometheus prometheus-node-exporter -y
+#apt-get install -y adduser libfontconfig1 musl
 
 #cd /home/user
 #unzip elk_8.9_deb-224190-ae521e.zip
-cd /home/user/basic_ubuntu_diplom
-bash netplan_elk.sh
-hostnamectl set-hostname elk
-reboot
+#cd /home/user/basic_ubuntu_diplom
+#bash netplan_elk.sh
+#hostnamectl set-hostname elk
+#reboot
 
 
 cd /home/user
 unzip elk_8.9_deb-224190-ae521e.zip
-
+#cd /home/user/elk_8.9-deb
 bash install_grafana.sh
 
 bash install_elastic.sh
