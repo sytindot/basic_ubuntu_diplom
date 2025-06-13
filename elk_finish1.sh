e8242634c0241368d0860401802153750357891d
kokdot <kok.74@mail.ru>
2025-06-13 09:24:14 +0500

    status

--- elk_finish1.sh
+++ elk_finish1.sh
@@ -13,11 +13,13 @@ cp kibana.yml /etc/kibana
 cp logstash.yml /etc/logstash
 
 cp logstash-nginx-es.conf /etc/logstash/conf.d
-sudo systemctl enable --now prometheus.service prometheus-node-exporter.service grafana-server.service kibana.service logstash.service elasticsearch.service
-#systemctl enable --now grafana-server.service
-#systemctl enable --now kibana.service
-#systemctl enable --now logstash.service
-#systemctl enable --now elasticsearch.service
+#sudo systemctl enable --now prometheus.service prometheus-node-exporter.service grafana-server.service kibana.service logstash.service elasticsearch.service
+systemctl enable --now prometheus
+systemctl enable --now prometheus-node-exporter
+systemctl enable --now grafana-server
+systemctl enable --now kibana
+systemctl enable --now logstash
+systemctl enable --now elasticsearch
 systemctl daemon-reload
 
 sudo systemctl restart  prometheus.service prometheus-node-exporter.service grafana-server.service kibana.service logstash.service elasticsearch.service
