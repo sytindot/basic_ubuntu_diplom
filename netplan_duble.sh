86b10525003a7a0a8029f78870d8838865e38750
kokdot <kok.74@mail.ru>
2025-06-13 11:44:52 +0500

    status

---
+++ netplan_duble.sh
@@ -0,0 +1,24 @@
+#!/bin/bash +x
+
+netplan_file='netplan/9999-network5.yaml'
+cd /home/user/basic_ubuntu_diplom
+if (( "$?" != 0 )); then
+	echo "Ошибка при попытке перехода в каталог basic_ubuntu_diplom"
+	exit 1
+fi
+# echo "check file netplan_file: " $netplan_file
+# echo "$(ls $netplan_file)"
+if [ -f "$netplan_file" ]; then
+	rm /etc/netplan/*
+	cp $netplan_file /etc/netplan
+	if (( "$?" == 0 )); then
+		netplan apply
+		#sleep 5
+		netplan generate
+	fi
+
+#	echo $netplan_file "успешно скопирован"
+else 
+	echo "Обибка при копировании файла $netplan_file" 
+fi
+
