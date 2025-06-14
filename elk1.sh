7e2376fcf8b9e7b6bdfa1de8d336da32d17cf055
kokdot <kok.74@mail.ru>
2025-06-14 21:59:26 +0500

    elk1

--- elk1.sh
+++ elk1.sh
@@ -1,29 +1,20 @@
 !/bin/bash
-#apt install default-jdk -y
-#apt install nginx -y
-#apt install unzip -y
-#apt install prometheus prometheus-node-exporter -y
-#apt-get install -y adduser libfontconfig1 musl
-
-#cd /home/user
-#unzip elk_8.9_deb-224190-ae521e.zip
-#cd /home/user/basic_ubuntu_diplom
-#bash netplan_elk.sh
-#hostnamectl set-hostname elk
-#reboot
 
 
 cd /home/user
 unzip elk_8.9_deb-224190-ae521e.zip
-cd /home/user/basic_ubuntu_diplom
-bash install_grafana.sh
 
 cd /home/user/basic_ubuntu_diplom
 bash install_elastic.sh
+bash config_elastic_base.sh
 
-cd /home/user/basic_ubuntu_diplom
 bash install_kibana.sh
+bash config_kibana.sh
 
-cd /home/user/basic_ubuntu_diplom
 bash install_logstash.sh
+bash config_logstash.sh
+
+bash install_grafana.sh
+
+bash config_elastic.sh
 
