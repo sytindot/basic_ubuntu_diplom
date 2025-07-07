cf170e783b1b82766dbbca2879ec72597138998e
kokdot <kok.74@mail.ru>
2025-07-07 21:37:12 +0500

    asdf

--- apache_conf.sh
+++ apache_conf.sh
@@ -2,8 +2,7 @@
 
 cp apache2/ports.conf /etc/apache2
 cp apache2/sites-available/* /etc/apache2/sites-available/
-cd /etc/apache2/sites-enabled
-rm *
+cd /etc/apache2/sites-enabled && rm *
 ln -s ../sites-available/000-default.conf 000-default.conf
 ln -s ../sites-available/001-default.conf 001-default.conf
 ln -s ../sites-available/002-default.conf 002-default.conf
